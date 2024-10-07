4aa3fb107596f2c3a6369889c3b66c826142be60
jinho shin <jinho.shin1@cj.net>
2024-10-08 08:53:13 +0900

    chore: naming Re:store

--- app/root.tsx
+++ app/root.tsx
@@ -19,6 +19,7 @@ export default function App() {
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Re:store</title>
           <Meta />
           <Links />
           <Scripts />
